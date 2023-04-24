a93066b3fd8e1ecdfcc1c00f535d8896f8806546
KayT <126052068+KayTsujita@users.noreply.github.com>
2023-04-24 23:12:59 +0900

    documentモデルバリデーション記述

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,8 @@
 class Document < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
+  has_one_attached :image
+
+  validates :document_title,     presence: true
+  validates :description,          presence: true
 end
